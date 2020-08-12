dfcc2badcbedcb0abb49e7402a00fed5bab8e810
miipart <marcus.forsberg@protonmail.com>
2020-08-12 20:50:31 +0200

    Added basic functionality for adding and viewing diffrent categories/boxes

--- Server/inv-mng/__init__.py
+++ Server/inv-mng/__init__.py
@@ -1,12 +1,34 @@
 
-from flask import Flask
+from flask import Flask, request, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    from .db import get_db
-    db = get_db()
-    #db['test'].insert_one({'name':'johnny'})
-    x = db['test'].find_one()
-    return x['name']
+    #from .db import add_category, get_categories
+    return "Hello"
+
+    #add_category("Packet one", "Run baby run")
+
+    #return ''.join( [f"<li>{x['name']}</li>" for x in get_categories()])
+
+@app.route('/api/new_category', methods=['GET', 'POST'])
+def api_new_category():
+    if request.method == "GET":
+        return render_template('/new_category.html')
+
+    from .db import add_category
+
+    new_category_name = request.form['name']
+    new_cateogry_desc = request.form['desc']
+
+    add_category(new_category_name, new_cateogry_desc)
+    return "ok"
+
+@app.route('/api/get_categories')
+def api_get_categories():
+    from .db import get_categories
+    from flask import jsonify
+    categories = [ {"name":x['name'], "id":str(x.get('_id')), "desc":x['desc'] } for x in get_categories() ]
+
+    return jsonify(categories)
--- Server/inv-mng/db.py
+++ Server/inv-mng/db.py
@@ -2,7 +2,25 @@ import pymongo
 from flask import current_app, g
 from flask.cli import with_appcontext
 
-def get_db():
+def __get_db():
     if 'db' not in g:
         g.db = pymongo.MongoClient('mongodb://server:fixMe@localhost:27017/Inventory-Manager')['Inventory-Manager']
     return g.db
+
+def __insert(collection, data):
+    db = __get_db()
+    col = db[collection]
+    r = col.insert_one(data)
+    return r
+
+def add_category(name="Sample Name. Please change", desc="A sample category. Please change"):
+    data= {"name":name, "desc":desc}
+    collection = "Category"
+    r = __insert(collection,data)
+    return r
+
+def get_categories():
+    db = __get_db()
+    collection = db['Category']
+    r = collection.find({})
+    return r
